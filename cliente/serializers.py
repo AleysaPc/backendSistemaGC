901b6c18595cd74f8f62f34b6653256b5d0db910
Ale <cruzmaralis17@gmail.com>
2025-03-24 23:47:21 -0400

    Cliente Serializers

--- cliente/serializers.py
+++ cliente/serializers.py
@@ -2,9 +2,18 @@ from .models import Cliente,Institucion
 from rest_framework import serializers
 
 class ClienteSerializer(serializers.ModelSerializer):
+
+    nombre_institucion = serializers.CharField(source="institucion.nombre", read_only=True)
     class Meta:
         model = Cliente
-        fields = '__all__'
+        fields = [
+            "nombre",
+            "apellido",
+            "cargo",
+            "email",
+            "nombre_institucion",
+            "institucion"
+        ]
 
 class InstitucionSerializer(serializers.ModelSerializer):
     class Meta:
