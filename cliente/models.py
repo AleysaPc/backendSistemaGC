a6765a1a7e084d6065a4b0b4a8c5d8696fb6e783
AleysaPc <cruzmaralis17@gmail.com>
2025-03-30 23:25:30 -0400

    Model cliente

--- cliente/models.py
+++ cliente/models.py
@@ -3,7 +3,7 @@ from django.db import models
 # Create your models here.
 class Institucion (models.Model):
     id_institucion = models.AutoField(primary_key=True)
-    nombre = models.CharField(max_length=100)
+    nombre_institucion = models.CharField(max_length=100)
     direccion = models.CharField(max_length=255)
     telefono = models.CharField(max_length=20)
 
